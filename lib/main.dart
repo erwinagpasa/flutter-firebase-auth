8f481afeee52f8022a7479cbefa59c62225c42e5
erwinagpasa <rootaccess000@gmail.com>
2022-12-26 05:16:54 +0300

    adding screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,106 +1,282 @@
+import 'package:firebase_auth/firebase_auth.dart'
+    hide PhoneAuthProvider, EmailAuthProvider;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
+import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart';
+import 'package:firebase_ui_oauth_facebook/firebase_ui_oauth_facebook.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:firebase_ui_oauth_twitter/firebase_ui_oauth_twitter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'config.dart';
+import 'decorations.dart';
+import 'firebase_options.dart';
+
+final actionCodeSettings = ActionCodeSettings(
+  url: 'https://flutterfire-e2e-tests.firebaseapp.com',
+  handleCodeInApp: true,
+  androidMinimumVersion: '1',
+  androidPackageName: 'io.flutter.plugins.firebase_ui.firebase_ui_example',
+  iOSBundleId: 'io.flutter.plugins.fireabaseUiExample',
+);
+final emailLinkProviderConfig = EmailLinkAuthProvider(
+  actionCodeSettings: actionCodeSettings,
+);
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  if (FirebaseAuth.instance.currentUser == null) {
+    await FirebaseAuth.instance.signInAnonymously();
+  }
+
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    emailLinkProviderConfig,
+    PhoneAuthProvider(),
+    GoogleProvider(clientId: GOOGLE_CLIENT_ID),
+    AppleProvider(),
+    FacebookProvider(clientId: FACEBOOK_CLIENT_ID),
+    TwitterProvider(
+      apiKey: TWITTER_API_KEY,
+      apiSecretKey: TWITTER_API_SECRET_KEY,
+      redirectUri: TWITTER_REDIRECT_URI,
+    ),
+  ]);
+
+  runApp(const FirebaseAuthUIExample());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// Overrides a label for en locale
+// To add localization for a custom language follow the guide here:
+// https://flutter.dev/docs/development/accessibility-and-localization/internationalization#an-alternative-class-for-the-apps-localized-resources
+class LabelOverrides extends DefaultLocalizations {
+  const LabelOverrides();
 
-  // This widget is the root of your application.
+  @override
+  String get emailInputLabel => 'Enter your email';
+}
+
+class FirebaseAuthUIExample extends StatelessWidget {
+  const FirebaseAuthUIExample({Key? key}) : super(key: key);
+
+  String get initialRoute {
+    final auth = FirebaseAuth.instance;
+
+    if (auth.currentUser == null || auth.currentUser!.isAnonymous) {
+      return '/';
+    }
+
+    if (!auth.currentUser!.emailVerified && auth.currentUser!.email != null) {
+      return '/verify-email';
+    }
+
+    return '/profile';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    final buttonStyle = ButtonStyle(
+      padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    final mfaAction = AuthStateChangeAction<MFARequired>(
+      (context, state) async {
+        final nav = Navigator.of(context);
 
-  final String title;
+        await startMFAVerification(
+          resolver: state.resolver,
+          context: context,
+        );
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+        nav.pushReplacementNamed('/profile');
+      },
+    );
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.standard,
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(style: buttonStyle),
+        textButtonTheme: TextButtonThemeData(style: buttonStyle),
+        outlinedButtonTheme: OutlinedButtonThemeData(style: buttonStyle),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      initialRoute: initialRoute,
+      routes: {
+        '/': (context) {
+          return SignInScreen(
+            actions: [
+              ForgotPasswordAction((context, email) {
+                Navigator.pushNamed(
+                  context,
+                  '/forgot-password',
+                  arguments: {'email': email},
+                );
+              }),
+              VerifyPhoneAction((context, _) {
+                Navigator.pushNamed(context, '/phone');
+              }),
+              AuthStateChangeAction<SignedIn>((context, state) {
+                if (!state.user!.emailVerified) {
+                  Navigator.pushNamed(context, '/verify-email');
+                } else {
+                  Navigator.pushReplacementNamed(context, '/profile');
+                }
+              }),
+              AuthStateChangeAction<UserCreated>((context, state) {
+                if (!state.credential.user!.emailVerified) {
+                  Navigator.pushNamed(context, '/verify-email');
+                } else {
+                  Navigator.pushReplacementNamed(context, '/profile');
+                }
+              }),
+              AuthStateChangeAction<CredentialLinked>((context, state) {
+                if (!state.user.emailVerified) {
+                  Navigator.pushNamed(context, '/verify-email');
+                } else {
+                  Navigator.pushReplacementNamed(context, '/profile');
+                }
+              }),
+              mfaAction,
+              EmailLinkSignInAction((context) {
+                Navigator.pushReplacementNamed(context, '/email-link-sign-in');
+              }),
+            ],
+            styles: const {
+              EmailFormStyle(signInButtonVariant: ButtonVariant.filled),
+            },
+            headerBuilder: headerImage('assets/images/flutterfire_logo.png'),
+            sideBuilder: sideImage('assets/images/flutterfire_logo.png'),
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  action == AuthAction.signIn
+                      ? 'Welcome to Firebase UI! Please sign in to continue.'
+                      : 'Welcome to Firebase UI! Please create an account to continue',
+                ),
+              );
+            },
+            footerBuilder: (context, action) {
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(
+                    action == AuthAction.signIn
+                        ? 'By signing in, you agree to our terms and conditions.'
+                        : 'By registering, you agree to our terms and conditions.',
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        '/verify-email': (context) {
+          return EmailVerificationScreen(
+            headerBuilder: headerIcon(Icons.verified),
+            sideBuilder: sideIcon(Icons.verified),
+            actionCodeSettings: actionCodeSettings,
+            actions: [
+              EmailVerifiedAction(() {
+                Navigator.pushReplacementNamed(context, '/profile');
+              }),
+              AuthCancelledAction((context) {
+                FirebaseUIAuth.signOut(context: context);
+                Navigator.pushReplacementNamed(context, '/');
+              }),
+            ],
+          );
+        },
+        '/phone': (context) {
+          return PhoneInputScreen(
+            actions: [
+              SMSCodeRequestedAction((context, action, flowKey, phone) {
+                Navigator.of(context).pushReplacementNamed(
+                  '/sms',
+                  arguments: {
+                    'action': action,
+                    'flowKey': flowKey,
+                    'phone': phone,
+                  },
+                );
+              }),
+            ],
+            headerBuilder: headerIcon(Icons.phone),
+            sideBuilder: sideIcon(Icons.phone),
+          );
+        },
+        '/sms': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments
+              as Map<String, dynamic>?;
+
+          return SMSCodeInputScreen(
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.of(context).pushReplacementNamed('/profile');
+              })
+            ],
+            flowKey: arguments?['flowKey'],
+            action: arguments?['action'],
+            headerBuilder: headerIcon(Icons.sms_outlined),
+            sideBuilder: sideIcon(Icons.sms_outlined),
+          );
+        },
+        '/forgot-password': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments
+              as Map<String, dynamic>?;
+
+          return ForgotPasswordScreen(
+            email: arguments?['email'],
+            headerMaxExtent: 200,
+            headerBuilder: headerIcon(Icons.lock),
+            sideBuilder: sideIcon(Icons.lock),
+          );
+        },
+        '/email-link-sign-in': (context) {
+          return EmailLinkSignInScreen(
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/');
+              }),
+            ],
+            provider: emailLinkProviderConfig,
+            headerMaxExtent: 200,
+            headerBuilder: headerIcon(Icons.link),
+            sideBuilder: sideIcon(Icons.link),
+          );
+        },
+        '/profile': (context) {
+          return ProfileScreen(
+            actions: [
+              SignedOutAction((context) {
+                Navigator.pushReplacementNamed(context, '/');
+              }),
+              mfaAction,
+            ],
+            actionCodeSettings: actionCodeSettings,
+            showMFATile: true,
+          );
+        },
+      },
+      title: 'Firebase UI demo',
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('en'),
+      localizationsDelegates: [
+        FirebaseUILocalizations.withDefaultOverrides(const LabelOverrides()),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        FirebaseUILocalizations.delegate,
+      ],
     );
   }
 }
